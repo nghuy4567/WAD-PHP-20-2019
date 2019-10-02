2ce740e30933dd7deda0181bb5f4a899bd89014a
huy <nghuy4567@gmail.com>
2019-10-02 20:45:24 +0700

    new

--- BT11.php
+++ BT11.php
@@ -1,15 +1,3 @@
 <?php
-  function tamgiac(){
-    $n = 3;
-    for ($i = 1; $i <= n ; $i++) { 
-      for ($j = 1; $j <=n ; $j++) {
-          echo " " ; 
-        if ($j = 1; $j <= 2*i ; $j ++) { 
-          echo "*" ;
-          echo "<br>";
-        }
-      }
-    }
-    tamgiac();
-  }
+ 
 ?>
--- BT7.php
+++ BT7.php
@@ -1,18 +1,5 @@
- 	<table>
+ 	
   <?php
-  for ($row = 1; $row <= 8 ; $row ++) { 
-    echo "<tr>";
-    for ($col = 0; $col <= 8 ; $col ++) { 
-      $total = $row + $col;
-      if ($total %2 == 0) {
-        echo "<td  height=30px width=30px bgcolor=#FFFFFF></td>";
-      } else {
-        echo "<td  height=30px width=30px bgcolor=#000000></td>";
-      }
-      
-    }
-    echo "</tr>";
-  }
+  
   
 ?>
-</table>
